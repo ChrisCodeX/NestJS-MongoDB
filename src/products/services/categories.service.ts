f98ac2f5927e6270ec759b5c4005d03859600f21
Christian <c.espinoza.code@gmail.com>
2023-01-20 17:25:44 -0500

    categoryes service implemented

--- src/products/services/categories.service.ts
+++ src/products/services/categories.service.ts
@@ -7,52 +7,44 @@ import { Category } from 'src/products/entities/category.entity';
 
 @Injectable()
 export class CategoriesService {
-  private counterId = 1;
-  private categories: Category[] = [
-    {
-      id: 1,
-      name: 'phones',
-    },
-  ];
+  // findAll() {
+  //   return this.categories;
+  // }
 
-  findAll() {
-    return this.categories;
-  }
+  // findOne(id: number) {
+  //   const categoryId = this.categories.findIndex((item) => item.id === id);
+  //   if (categoryId === -1) {
+  //     throw new NotFoundException(`category ${id} not found`);
+  //   }
+  //   return this.categories[categoryId];
+  // }
 
-  findOne(id: number) {
-    const categoryId = this.categories.findIndex((item) => item.id === id);
-    if (categoryId === -1) {
-      throw new NotFoundException(`category ${id} not found`);
-    }
-    return this.categories[categoryId];
-  }
+  // create(payload: CreateCategoryDto) {
+  //   this.counterId += 1;
+  //   const newCategory = {
+  //     id: this.counterId,
+  //     ...payload,
+  //   };
+  //   this.categories.push(newCategory);
+  //   return newCategory;
+  // }
 
-  create(payload: CreateCategoryDto) {
-    this.counterId += 1;
-    const newCategory = {
-      id: this.counterId,
-      ...payload,
-    };
-    this.categories.push(newCategory);
-    return newCategory;
-  }
+  // update(id: number, payload: UpdateCategoryDto) {
+  //   const category = this.findOne(id);
+  //   const categoryIndex = this.categories.findIndex((item) => item.id === id);
+  //   this.categories[categoryIndex] = {
+  //     ...category,
+  //     ...payload,
+  //   };
+  //   return this.categories[categoryIndex];
+  // }
 
-  update(id: number, payload: UpdateCategoryDto) {
-    const category = this.findOne(id);
-    const categoryIndex = this.categories.findIndex((item) => item.id === id);
-    this.categories[categoryIndex] = {
-      ...category,
-      ...payload,
-    };
-    return this.categories[categoryIndex];
-  }
-
-  remove(id: number) {
-    const categoryId = this.categories.findIndex((item) => item.id === id);
-    if (categoryId === -1) {
-      throw new NotFoundException(`category ${id} not found`);
-    }
-    this.categories.splice(categoryId, 1);
-    return true;
-  }
+  // remove(id: number) {
+  //   const categoryId = this.categories.findIndex((item) => item.id === id);
+  //   if (categoryId === -1) {
+  //     throw new NotFoundException(`category ${id} not found`);
+  //   }
+  //   this.categories.splice(categoryId, 1);
+  //   return true;
+  // }
 }
