6904616774862aa68f4fe287a543279bcd36153a
Christian <c.espinoza.code@gmail.com>
2023-01-20 17:26:41 -0500

    category entity implemented

--- src/products/entities/category.entity.ts
+++ src/products/entities/category.entity.ts
@@ -1,4 +1,13 @@
-export class Category {
-  id: number;
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+@Schema()
+export class Category extends Document {
+  @Prop({ type: String })
   name: string;
+
+  @Prop({ type: String })
+  image: string;
 }
+
+export const CategorySchema = SchemaFactory.createForClass(Category);
