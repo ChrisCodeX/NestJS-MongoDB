09cbdd01aea79d8e43860d7ffc283a98f64900a7
Christian <c.espinoza.code@gmail.com>
2023-01-19 23:06:00 -0500

    getTasks method implemented in app service for tests

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,10 +1,12 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { ConfigType } from '@nestjs/config';
 import config from 'config';
+import { Db } from 'mongodb';
 
 @Injectable()
 export class AppService {
   constructor(
+    // @Inject('MONGO') private database: Db,
     @Inject(config.KEY) private configService: ConfigType<typeof config>,
   ) {}
 
@@ -13,5 +15,10 @@ export class AppService {
     const dbName = this.configService.database.name;
     console.log(apiKey);
     return `Hello World! ${apiKey} - ${dbName}`;
+  }
+
+  getTasks() {
+    // const taskCollection = this.database.collection('tasks');
+    // return taskCollection.find().toArray();
   }
 }
