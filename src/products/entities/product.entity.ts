f3f36f8b25e62af791d4395c83b4f635460b9014
Christian <c.espinoza.code@gmail.com>
2023-01-19 23:00:47 -0500

    product document schema created

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,8 +1,22 @@
-export interface Product {
-  id: number;
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+@Schema()
+export class Product extends Document {
+  @Prop({ type: String, required: true })
   name: string;
+
+  @Prop({ type: String })
   description: string;
+
+  @Prop({ type: Number })
   price: number;
+
+  @Prop({ type: Number })
   stock: number;
+
+  @Prop({ type: String })
   image: string;
 }
+
+export const ProductSchema = SchemaFactory.createForClass(Product);
