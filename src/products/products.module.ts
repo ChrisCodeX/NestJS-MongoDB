dc6ce433b983d9fb81aaaaf7bd15ff9ab3fe3eee
Christian <c.espinoza.code@gmail.com>
2023-01-19 23:02:32 -0500

    MongooseModule injected in products module

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,12 +1,23 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { BrandsController } from './controllers/brands.controller';
 import { CategoriesController } from './controllers/categories.controller';
 import { ProductsController } from './controllers/products.controller';
 import { BrandsService } from './services/brands.service';
 import { CategoriesService } from './services/categories.service';
 import { ProductsService } from './services/products.service';
+import { ProductSchema, Product } from 'src/products/entities/product.entity';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: Product.name,
+        schema: ProductSchema,
+      },
+    ]),
+  ],
   controllers: [ProductsController, BrandsController, CategoriesController],
   providers: [ProductsService, BrandsService, CategoriesService],
   exports: [ProductsService],
