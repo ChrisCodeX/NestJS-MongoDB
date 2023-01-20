b1f3ca5482fab8564a487e6eb7ad810ab6cdee3a
Christian <c.espinoza.code@gmail.com>
2023-01-19 22:58:28 -0500

    Mongoose module imported

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,15 +1,51 @@
 import { Module, Global } from '@nestjs/common';
+import { MongoClient } from 'mongodb';
+import { ConfigType } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
+import config from 'config';
 
 const API_KEY_DEV = 'dev_123';
 const API_KEY_PROD = 'prod_321';
 
 @Global()
 @Module({
+  imports: [
+    MongooseModule.forRootAsync({
+      useFactory: async (configService: ConfigType<typeof config>) => {
+        // Mongo Configurations from config
+        const { connection, user, password, port, host, dbName } =
+          configService.mongo;
+        return {
+          uri: `${connection}://${host}:${port}`,
+          user,
+          pass: password,
+          dbName,
+        };
+      },
+      inject: [config.KEY],
+    }),
+  ],
   providers: [
     {
       provide: 'API_KEY',
       useValue: process.env.NODE_ENV === 'dev' ? API_KEY_DEV : API_KEY_PROD,
     },
+    // {
+    //   provide: 'MONGO',
+    //   useFactory: async (configService: ConfigType<typeof config>) => {
+    //     // Mongo Configurations from config
+    //     const { connection, user, password, port, host, dbName } =
+    //       configService.mongo;
+
+    //     const uri = `${connection}://${user}:${password}@${host}:${port}/?authMechanism=DEFAULT&tls=false`;
+    //     const client = new MongoClient(uri);
+    //     await client.connect();
+    //     const database = client.db(`${dbName}`);
+    //     return database;
+    //   },
+    //   inject: [config.KEY],
+    // },
   ],
   exports: ['API_KEY'],
 })
