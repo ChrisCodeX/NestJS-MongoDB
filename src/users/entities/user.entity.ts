2ab526eb565de43e18f4876b0862b293b0c8c25c
Christian <c.espinoza.code@gmail.com>
2023-01-21 12:17:46 -0500

    user entity created as Schema model

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,6 +1,16 @@
-export class User {
-  id: number;
+import { Schema, Prop, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+@Schema()
+export class User extends Document {
+  @Prop({ type: String })
   email: string;
+
+  @Prop({ type: String })
   password: string;
+
+  @Prop({ type: String })
   role: string;
 }
+
+export const UserSchema = SchemaFactory.createForClass(User);
