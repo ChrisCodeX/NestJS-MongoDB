d4d54a9e33bc2869179ba8a50398e2431fd4648c
Christian <c.espinoza.code@gmail.com>
2023-01-20 20:49:56 -0500

    brands controllers connected with brands services

--- src/products/controllers/brands.controller.ts
+++ src/products/controllers/brands.controller.ts
@@ -2,13 +2,13 @@ import {
   Controller,
   Get,
   Param,
-  ParseIntPipe,
   Post,
   Body,
   Patch,
   Delete,
 } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
+import { MongoIdPipe } from 'src/common/mongo-id/mongo-id.pipe';
 import { CreateBrandDto, UpdateBrandDto } from 'src/products/dtos/brands.dto';
 
 import { BrandsService } from 'src/products/services/brands.service';
@@ -21,47 +21,44 @@ export class BrandsController {
   /* Get Methods */
   // Get all brands
   @Get()
-  getBrands() {
+  async getBrands() {
     return {
-      message: this.brandsService.findAll(),
+      message: await this.brandsService.findAll(),
     };
   }
 
   // Get a brand by id
   @Get('/:id')
-  getBrand(@Param('id', ParseIntPipe) id: number) {
+  async getBrand(@Param('id', MongoIdPipe) id: string) {
     return {
-      message: this.brandsService.findOne(id),
+      message: await this.brandsService.findOne(id),
     };
   }
 
   /* Post Methods */
   // Create a new brand
   @Post()
-  create(@Body() payload: CreateBrandDto) {
+  async create(@Body() payload: CreateBrandDto) {
     return {
-      message: this.brandsService.create(payload),
+      message: await this.brandsService.create(payload),
     };
   }
 
   /* Patch Methods */
   // Update brand
   @Patch('/:id')
-  patch(
-    @Param('id', ParseIntPipe) id: number,
-    @Body() payload: UpdateBrandDto,
-  ) {
+  async patch(@Param('id') id: string, @Body() payload: UpdateBrandDto) {
     return {
-      message: this.brandsService.update(id, payload),
+      message: await this.brandsService.update(id, payload),
     };
   }
 
   /* Delete Methods */
   // Delete a brand
   @Delete('/:id')
-  delete(@Param('id', ParseIntPipe) id: number) {
+  async delete(@Param('id') id: string) {
     return {
-      message: this.brandsService.delete(id),
+      message: await this.brandsService.remove(id),
     };
   }
 }
