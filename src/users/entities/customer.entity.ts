44a3221c0fa881d939c33d33cdc82f58386e5cef
Christian <c.espinoza.code@gmail.com>
2023-01-21 17:39:11 -0500

    customer entity created

--- src/users/entities/customer.entity.ts
+++ src/users/entities/customer.entity.ts
@@ -1,6 +1,16 @@
-export class Customer {
-  id: number;
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+@Schema()
+export class Customer extends Document {
+  @Prop({ type: String })
   name: string;
+
+  @Prop({ type: String })
   lastName: string;
+
+  @Prop({ type: String })
   phone: string;
 }
+
+export const CustomerSchema = SchemaFactory.createForClass(Customer);
