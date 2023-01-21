f33f68e23a9137adaa4ebd9d1dfb6d59f5af030b
Christian <c.espinoza.code@gmail.com>
2023-01-21 17:42:02 -0500

    mongoose module imported in user module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,12 +1,22 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { ProductsModule } from 'src/products/products.module';
 import { CustomersController } from './controllers/customers.controller';
 import { UsersController } from './controllers/users.controller';
+import { Customer, CustomerSchema } from './entities/customer.entity';
 import { CustomersService } from './services/customers.service';
 import { UsersService } from './services/users.service';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: Customer.name,
+        schema: CustomerSchema,
+      },
+    ]),
+    ProductsModule,
+  ],
   controllers: [UsersController, CustomersController],
   providers: [UsersService, CustomersService],
 })
