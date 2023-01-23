ac2afd5cb47c7c2fd955e0551137686d5ac6b08e
Christian <c.espinoza.code@gmail.com>
2023-01-22 22:30:13 -0500

    skills dto created

--- src/users/dtos/subdocuments/skills.dto.ts
+++ src/users/dtos/subdocuments/skills.dto.ts
@@ -1,16 +1,14 @@
 import { Field, InputType } from '@nestjs/graphql';
-import { ApiProperty, PartialType } from '@nestjs/swagger';
+import { PartialType } from '@nestjs/swagger';
 import { IsString, IsNotEmpty } from 'class-validator';
 
 @InputType()
 export class CreateSkillDto {
-  @ApiProperty()
   @Field()
   @IsString()
   @IsNotEmpty()
   readonly name: string;
 
-  @ApiProperty()
   @Field()
   @IsString()
   @IsNotEmpty()
