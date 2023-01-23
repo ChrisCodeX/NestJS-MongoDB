f279b1c9a81692a1388282bcc790f7c1646fbd28
Christian <c.espinoza.code@gmail.com>
2023-01-22 22:39:51 -0500

    order entity modified - Order class now extends from Document

--- src/products/entities/order.entity.ts
+++ src/products/entities/order.entity.ts
@@ -1,8 +1,16 @@
 import { User } from 'src/users/entities/user.entity';
 import { Product } from 'src/products/entities/product.entity';
+import { Document } from 'mongoose';
+import { Prop, SchemaFactory } from '@nestjs/mongoose';
 
-export class Order {
+export class Order extends Document {
+  @Prop({ type: Date })
   date: Date;
+
+  // @Prop({type})
   user: User;
+
   product: Product[];
 }
+
+export const OrderSchema = SchemaFactory.createForClass(Order);
